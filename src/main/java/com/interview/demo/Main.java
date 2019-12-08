f07acd7d51870ba564cadf36185951628a26b331
Daniela <druzmieres@gmail.com>
2019-12-08 14:49:23 -0300

    add main class

---
+++ src/main/java/com/interview/demo/Main.java
@@ -0,0 +1,17 @@
+package com.interview.demo;
+
+
+public class Main {
+
+    public static void main(String [] args)
+    {
+        // Create an item.
+        Item headphones = new Item("Bootleg headphones", 0.99);
+        // Print the name and price of the item.
+        System.out.printf("The item %s costs %s.%n", headphones.getName(), headphones.getPrice());
+        // Add customer comment.
+        String author = "Catalina Bachmann";
+        headphones.addComment(author, "Worst headphones ever >:(", 1);
+        System.out.printf("%s commented \"%s\"%n", author, headphones.getComment(author).toLowerCase());
+    }
+}
