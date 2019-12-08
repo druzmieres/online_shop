59482084ec27a8d163cfccb9ad413298158f57f1
Daniela <druzmieres@gmail.com>
2019-12-08 15:05:03 -0300

    improve format

--- src/main/java/com/interview/demo/Main.java
+++ src/main/java/com/interview/demo/Main.java
@@ -8,10 +8,11 @@ public class Main {
         // Create an item.
         Item headphones = new Item("Bootleg headphones", 0.99);
         // Print the name and price of the item.
-        System.out.printf("The item %s costs %s.%n", headphones.getName(), headphones.getPrice());
+        System.out.printf("The item \"%s\" costs %s.%n", headphones.getName().toLowerCase(), headphones.getPrice());
         // Add customer comment.
         String author = "Catalina Bachmann";
         headphones.addComment(author, "Worst headphones ever >:(", 1);
+        // Print comment
         System.out.printf("%s commented \"%s\"%n", author, headphones.getComment(author).toLowerCase());
     }
 }
