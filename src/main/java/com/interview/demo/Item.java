a9e4136cad90abd9b121230dbd57e3a2e6f1c4bf
Daniela <druzmieres@gmail.com>
2019-12-08 14:10:24 -0300

    add basic Item class

--- src/main/java/com/interview/demo/Item.java
+++ src/main/java/com/interview/demo/Item.java
@@ -1,13 +1,43 @@
 package com.interview.demo;
 
+import java.util.ArrayList;
+
 /**
- * Hello world!
+ * This class represents an item in an online shop.
+ * Item objects store the information of item, like name, price, etc.
  *
+ * @author Daniela Ruz
+ * @version 0.1
  */
-public class App 
+public class Item
 {
-    public static void main( String[] args )
+    private String name;
+    private double price; // in dollars
+    private ArrayList<Comment> comments;
+
+    /**
+     * Create a new sales item.
+     */
+    public Item(String name, double price)
     {
-        System.out.println( "Hello World!" );
+        this.name = name;
+        this.price = price;
+        comments = new ArrayList<>();
+    }
+
+    /**
+     * Return the name of this item.
+     */
+    public String getName()
+    {
+        return name;
+    }
+
+    /**
+     * Return the price of this item.
+     */
+    public double getPrice()
+    {
+        return price;
     }
 }
