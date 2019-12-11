bf5c21c2a637290b70ae6de8343e711cbe747154
Daniela <druzmieres@gmail.com>
2019-12-10 21:28:45 -0300

    replace prints by logs

--- src/main/java/com/interview/demo/Main.java
+++ src/main/java/com/interview/demo/Main.java
@@ -1,18 +1,24 @@
 package com.interview.demo;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
 
-    public static void main(String [] args)
-    {
+    public static void main(String[] args) {
+        // Create a Logger
+        Logger logger = Logger.getLogger(Main.class.getName());
+
         // Create an item.
         Item headphones = new Item("Bootleg headphones", 0.99);
         // Print the name and price of the item.
-        System.out.printf("The item \"%s\" costs $%s.%n", headphones.getName().toLowerCase(), headphones.getPrice());
+        logger.log(Level.INFO, String.format("The item %s costs $%s.%n",
+                headphones.getName().toLowerCase(), headphones.getPrice()));
         // Add customer comment.
         String author = "Catalina Bachmann";
         headphones.addComment(author, "Worst headphones ever >:(", 1);
         // Print comment
-        System.out.printf("%s commented \"%s.\"%n", author, headphones.getComment(author).toLowerCase());
+        logger.log(Level.INFO, String.format("%s commented \"%s.\"%n",
+                author, headphones.getComment(author).toLowerCase()));
     }
 }
